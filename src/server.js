24d80fda12d00f448242cac85509f68a3bf0356a
Judie Thai <judiethai.art@gmail.com>
2022-08-22 19:42:10 -0400

    maybe this'll work

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
-import { Server } from 'boardgame.io/server';
+import { Server, Origins } from 'boardgame.io/server';
 import { Omok } from './Game';
 import path from 'path';
 import serve from 'koa-static';
 
 const server = Server({
   games: [Omok],
+  origins: [Origins.LOCALHOST_IN_DEVELOPMENT,
+    'https://web-omok.herokuapp.com/',
+    'localhost']
 });
 
 const PORT = process.env.PORT || 8000;
