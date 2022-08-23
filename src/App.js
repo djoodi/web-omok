44ff5469dd28f75ae3ef3fd8ca162f833d4d95c7
Judie Thai <judiethai.art@gmail.com>
2022-08-23 18:53:43 -0400

    staging for heroku deployment

--- src/App.js
+++ src/App.js
@@ -1,72 +1,15 @@
-const  React = require('react');
-const { Lobby } = require('boardgame.io/react');
-const { Client } = require('boardgame.io/react');
-const { SocketIO } = require('boardgame.io/multiplayer');
-const { Omok } = require('./Game');
-const { OmokBoard } = require('./Board');
-
-const OmokClient = Client({ 
-  game: Omok,
-  board: OmokBoard,
-  //debug: false,
-  multiplayer: SocketIO({server: 'localhost:8000'}),
-});
+import React from 'react';
+import { Lobby } from 'boardgame.io/react';
+import { OmokBoard } from './board';
+import { Omok } from './game';
 
 const { protocol, hostname, port } = window.location;
 const server = `${protocol}//${hostname}:${port}`;
-console.log(server);
-const importedGames = [{game: Omok, board: OmokBoard}];
+const importedGames = [{ game: Omok, board: OmokBoard }];
 
-class App extends React.Component {
-  render() {
-    return (
-      <Lobby
-      gameServer={`http://${window.location.hostname}:8000`}
-      lobbyServer={`http://${window.location.hostname}:8000`}
-      gameComponents={[
-        { game: Omok, board: OmokBoard }
-      ]}
-    />)
-  }
-}
-
-export default App;
-
-
-// class App extends React.Component {
-//   state = { playerID: null };
-
-//   render() {
-//     if (this.state.playerID === null) {
-//       return (
-//         <div>
-//           <p>Play as</p>
-//           <button onClick={() => this.setState({ playerID: "0" })}>
-//             Player 0
-//           </button>
-//           <button onClick={() => this.setState({ playerID: "1" })}>
-//             Player 1
-//           </button>
-//         </div>
-//       );
-//     }
-//     return (
-//       <div>
-//         <TicTacToeClient playerID={this.state.playerID} />
-//       </div>
-//     );
-//   }
-// }
-
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <TicTacToeClient playerID = "0" />
-//         <TicTacToeClient playerID = "1" />
-//       </div>
-  
-//     )
-//   }
-// }
+export default () => (
+  <div>
+    <h1>Lobby</h1>
+    <Lobby gameServer={server} lobbyServer={server} gameComponents={importedGames} />
+  </div>
+);
--- src/server.js
+++ src/server.js
@@ -1,26 +1,20 @@
-import { Server, Origins } from 'boardgame.io/server';
-import { Omok } from './Game';
+import { Server } from 'boardgame.io/server';
 import path from 'path';
 import serve from 'koa-static';
+import { Omok } from './game';
 
-const server = Server({
-  games: [Omok],
-  origins: [Origins.LOCALHOST]
+const server = Server({ games: [Omok] });
+const PORT = process.env.PORT || 8000;
+
+// Build path relative to the server.js file
+const frontEndAppBuildPath = path.resolve(__dirname, './build');
+server.app.use(serve(frontEndAppBuildPath))
+
+server.run(PORT, () => {
+  server.app.use(
+    async (ctx, next) => await serve(frontEndAppBuildPath)(
+      Object.assign(ctx, { path: 'index.html' }),
+      next
+    )
+  )
 });
-
-server.run(8000);
-
-// const PORT = process.env.PORT || 8000;
-
-// const frontEndAppBuildPath = path.resolve(__dirname, './build');
-// server.app.use(serve(frontEndAppBuildPath))
-
-// server.run(PORT, () => {
-//   console.log("running server on " + PORT);
-//   server.app.use(
-//     async (ctx, next) => await serve(frontEndAppBuildPath)(
-//       Object.assign(ctx, { path: 'index.html' }),
-//       next()
-//     )
-//   );
-// });
